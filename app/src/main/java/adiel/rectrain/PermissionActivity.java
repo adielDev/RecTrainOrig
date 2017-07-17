5da0e94b964fc63f6926e56de16a4db99fa318bb
adiel <adieldev@gmail.com>
2017-07-17 12:19:22 +0300

    improve grant permission process

--- app/src/main/java/adiel/rectrain/PermissionActivity.java
+++ app/src/main/java/adiel/rectrain/PermissionActivity.java
@@ -3,36 +3,44 @@ package adiel.rectrain;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.constraint.solver.ArrayLinkedVariables;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class PermissionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_permission);
-        if (    ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-                &&
-                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-                &&
-                ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) == PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this, "good permission !!", Toast.LENGTH_SHORT).show();
-            startActivityMain();
 
-        }else {
-            Log.d("temp","no permission");
-            Toast.makeText(this, "no permission !!", Toast.LENGTH_SHORT).show();
-            String[] strings = new String[3];
-            strings[0] = Manifest.permission.WRITE_EXTERNAL_STORAGE;
-            strings[1] = Manifest.permission.READ_EXTERNAL_STORAGE;
-            strings[2] = Manifest.permission.RECEIVE_SMS;
-
-            ActivityCompat.requestPermissions(this,strings,100);
+        ArrayList<String> permissions = new ArrayList<>();
+        permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        permissions.add(Manifest.permission.RECEIVE_SMS);
+        permissions.add(Manifest.permission.INTERNET);
+        boolean permissionGranted=true;
+        for(String permission : permissions){
+            permissionGranted = (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED);
+            if(!permissionGranted){
+                break;
+            }
         }
+        if(permissionGranted) {
+            startActivityMain();
+            finish();
+        }else {
+            String[] permissionArray = new String[permissions.size()];
+            for (int i = 0; i <permissions.size() ; i++) {
+                permissionArray[i] = permissions.get(i);
+            }
+            ActivityCompat.requestPermissions(this,permissionArray,100);
+        }
+
     }
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
